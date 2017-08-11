906f1e3de0c41b3577fce8336f154e6e67b80c62
xyduan <xyduan@thoughtworks.com>
2017-08-11 10:59:37 +0800

    update config

--- src/main/java/com/thoughtworks/retail/config/Application.java
+++ src/main/java/com/thoughtworks/retail/config/Application.java
@@ -2,13 +2,14 @@ package com.thoughtworks.retail.config;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
 @EnableDiscoveryClient
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
     public static void main(String[] args){
         SpringApplication.run(Application.class, args);
